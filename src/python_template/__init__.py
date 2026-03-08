e1d61851559685cbd35527eb19765e7ac2a94573
Javier Carnero <jcarnero@plainconcepts.com>
2026-03-08 21:41:04 +0100

    ✨ pydantic settings

--- src/python_template/__init__.py
+++ src/python_template/__init__.py
@@ -1,6 +1,11 @@
 from loguru import logger
 
+from python_template.settings import app_settings
+
 
 def main() -> int:
-    logger.info("Hello from python-template!")
+    logger.info(
+        "Hello from python-template! sample_variable={value}",
+        value=app_settings.MY_SAMPLE_VARIABLE,
+    )
     return 0
---
+++ src/python_template/settings.py
@@ -0,0 +1,13 @@
+from typing import ClassVar
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AppSettings(BaseSettings):
+    model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
+        env_file="dev.env", env_file_encoding="utf-8"
+    )
+    MY_SAMPLE_VARIABLE: str = "DEFAULT_VALUE"
+
+
+app_settings = AppSettings()
